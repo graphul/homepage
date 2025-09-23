b894afdf0fc5906003bc86025c9b7141f676ec5a
graphul <info@graphul.jp>
2025-09-23 10:26:12 +0900

    Remove site.webmanifest to fix favicon conflict

--- blog-build/assets/js/data/swconf.js
+++ blog-build/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1757782942',resources: [
+    cacheName: 'chirpy-1758590714',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
