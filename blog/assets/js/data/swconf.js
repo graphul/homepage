ec9b247835fda3d460f0989d8aa20881e52c669f
graphul <graphul@users.noreply.github.com>
2025-09-26 12:20:12 +0000

    deploy: 1af53d7eb254019257b58ecc7bf4eb2eb2c7daa0

--- blog/assets/js/data/swconf.js
+++ blog/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1758888182',resources: [
+    cacheName: 'chirpy-1757555771',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
