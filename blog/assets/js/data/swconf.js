9b6c3a3ae92be276cd7a231aaf1ff191140bf82d
graphul <graphul@users.noreply.github.com>
2026-02-19 19:11:30 +0000

    deploy: c3b156c6baa5af05545101190b4a758ffa09fce4

---
+++ blog/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1771528288',resources: [
+      '/blog/assets/css/jekyll-theme-chirpy.css',
+      '/blog/',
+      
+        '/blog/categories/',
+      
+        '/blog/tags/',
+      
+        '/blog/archives/',
+      
+        '/blog/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
